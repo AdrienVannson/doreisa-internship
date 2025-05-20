0c0d7f3639fcbff0619d7691327d6d67d20eb3b1
Adrien Vannson <adrien.vannson@protonmail.com>
2025-05-20 12:50:09 +0200

    Fix slurm script

--- jean-zay/test/test.bash
+++ jean-zay/test/test.bash
@@ -14,6 +14,9 @@ scontrol show hostnames $SLURM_JOB_NODELIST > nodes.txt
 head -n 1 nodes.txt > headnode.txt
 tail -n +2 nodes.txt > workernodes.txt
 
+headnode=$(cat headnode.txt)
+workernodes=$(cat workernodes.txt | tr '\n' ',' | sed 's/,$//')
+
 srun --nodes=1 --nodelist=$headnode python3 head.py &
 
 srun --nodes=$((SLURM_JOB_NUM_NODES - 1)) --nodelist=$workernodes python3 worker.py $headnode &
