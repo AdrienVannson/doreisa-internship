11ea7edae4ae9b655d06dbca5e316f9ea0efb78c
Adrien Vannson <adrien.vannson@protonmail.com>
2025-06-03 16:23:22 +0200

    Fix on Jean Zay

--- experiments/02-distributed-scheduling/worker.py
+++ experiments/02-distributed-scheduling/worker.py
@@ -1,19 +1,17 @@
-import os
 import sys
 import time
 
 import numpy as np
+import ray
 
 from doreisa.simulation_node import Client
 
-if len(sys.argv) == 4:
-    rank, total, nb_chunks_of_node = int(sys.argv[1]), int(sys.argv[2]), int(sys.argv[3])
-else:
-    tasks_per_node = int(os.environ["SLURM_NTASKS_PER_NODE"])
-    rank, total, nb_chunks_of_node = int(os.environ["SLURM_PROCID"]), tasks_per_node * int(os.environ["SLURM_JOB_NUM_NODES"]), tasks_per_node
+rank, total, nb_chunks_of_node = int(sys.argv[1]), int(sys.argv[2]), int(sys.argv[3])
 
 with open("debug.txt", "a") as f:
     f.write(f"rank={rank} total={total} nb_chunks_of_node={nb_chunks_of_node}\n")
+
+ray.init(address="auto")
 
 client = Client()
 array = np.random.randint(0, 100, (10, 10), dtype=np.int64)
