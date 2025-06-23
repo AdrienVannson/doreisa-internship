710fa229f349937974d9e224bb5eeb04e3cb7171
Adrien Vannson <adrien.vannson@protonmail.com>
2025-06-23 16:36:10 +0200

    Update plot.py

--- experiments/02-distributed-scheduling/plot.py
+++ experiments/02-distributed-scheduling/plot.py
@@ -1,24 +1,42 @@
 import matplotlib.ticker
 from matplotlib import pyplot as plt
 
-data = """1 53.139193058013916 159.42504048347473
-2 53.48182678222656 175.57791948318481
-4 53.80112648010254 190.45238971710205
-8 53.88177990913391 212.52898335456848
-16 53.875473737716675 227.09651589393616
-32 67.72971630096436 252.02704548835754
-64 102.66593813896179 346.91654682159424"""
+# Doreisa v0.1.5
+data_1 = """1 170.16521334648132
+2 185.2690351009369
+4 195.1859700679779
+8 219.19524550437927
+16 244.32251572608948
+32 311.8170154094696
+64 453.9083671569824
+128 928.2888233661652
+255 2962.373"""  # Linear update from 254 to 255
+
+# Doreisa v0.1.6
+data_2 = """1 167.9294991493225
+2 187.20595002174377
+4 198.46129059791565
+8 211.9185733795166
+16 236.47691249847412
+32 297.59584069252014
+64 412.58087515830994
+128 739.9024617671967
+255 1429.899"""
 
 xs, ys1, ys2 = [], [], []
 
-for line in data.split("\n"):
-    x, y1, y2 = map(float, line.split(" "))
+for l1, l2 in zip(data_1.split("\n"), data_2.split("\n")):
+    x1, y1 = map(float, l1.split(" "))
+    x2, y2 = map(float, l2.split(" "))
+    assert x1 == x2
 
-    xs.append(x)
+    if x1 > 200:
+        break
+
+    xs.append(x1)
     ys1.append(y1)
     ys2.append(y2)
 
-# fig, (ax1, ax2) = plt.subplots(1, 2, figsize=(8, 4))
 fig, ax1 = plt.subplots(1, 1, figsize=(4, 3))
 
 # Total time per iteration
@@ -32,29 +50,11 @@ ax1.get_xaxis().set_major_formatter(matplotlib.ticker.ScalarFormatter())
 ax1.get_xaxis().minorticks_off()
 ax1.set_xticks(xs)
 
+ax1.plot(xs, ys1)
+ax1.scatter(xs, ys1)
+
 ax1.plot(xs, ys2)
 ax1.scatter(xs, ys2)
 
-# Proportions
-# ax2.set_xlabel("Number of chunks")
-# ax2.set_ylabel("Proportion of the total time (%)")
-
-# ax2.set_xscale("log")
-
-# ax2.get_xaxis().set_major_formatter(matplotlib.ticker.ScalarFormatter())
-# ax2.get_xaxis().minorticks_off()
-# ax2.set_xticks(xs)
-
-
-# ratio = [y1 / y2 * 100 for y1, y2 in zip(ys1, ys2)]
-
-# ax2.fill_between(xs, ratio, 100, alpha=0.4, facecolor="orange", label="Scheduling the tasks")
-# ax2.fill_between(xs, 0, ratio, alpha=0.4, facecolor="red", label="Building the array")
-# ax2.legend(loc="upper right")
-
-# ax2.plot(xs, ratio)
-# ax2.scatter(xs, ratio)
-
 plt.tight_layout()
-# plt.savefig("plot.png")
-plt.savefig("plot.svg")
+plt.savefig("exp-02-total-time-v0.1.5.svg")
