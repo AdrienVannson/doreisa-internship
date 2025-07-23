ce4a71e2fa00415455acb2a47bf48573280a7f76
Adrien Vannson <adrien.vannson@protonmail.com>
2025-07-23 11:34:05 +0200

    Remove Jean Zay

--- jean-zay/test/head.py
+++
@@ -1,8 +0,0 @@
-import os
-import socket
-
-dir_path = os.path.dirname(os.path.realpath(__file__))
-hostname = socket.gethostname()
-
-with open(f"{dir_path}/output-head.txt", "w") as f:
-    f.write(f"I'm {hostname}. I'm the head!\n")
