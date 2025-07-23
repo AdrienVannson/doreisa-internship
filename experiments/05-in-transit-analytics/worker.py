9434fd2860bae47fe503e2dbb9d0c198cce66864
Adrien Vannson <adrien.vannson@protonmail.com>
2025-07-23 16:28:52 +0200

    Add debug

--- experiments/05-in-transit-analytics/worker.py
+++ experiments/05-in-transit-analytics/worker.py
@@ -6,6 +6,8 @@ from doreisa.simulation_node import InTransitClient
 
 rank, total, nb_chunks_of_node = int(sys.argv[1]), int(sys.argv[2]), int(sys.argv[3])
 
+print("Starting worker with parameters:", sys.argv[1:])
+
 client = InTransitClient(analytic_node_address=sys.argv[4])
 array = np.random.randint(0, 100, (10, 10), dtype=np.int64)
 
